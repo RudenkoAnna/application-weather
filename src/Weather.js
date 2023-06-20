74a8666dee06332dccff3f7b9437850ec9fc9aec
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-20 16:03:13 +0300

    added some nice loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,25 @@
 import axios from "axios";
-export default function Weather() {
+import { RevolvingDot } from "react-loader-spinner";
+export default function Weather(props) {
   function handleResponse(response) {
     alert(`the weather in Kyiv is ${response.data.main.temp}C`);
   }
   let apiKey = "39d0e5ab9f18d4b08648c0969ea4cd9f";
   let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
 
   axios.get(apiUrl).then(handleResponse);
-  return <h2> Weather forecast </h2>;
+  return (
+    <RevolvingDot
+      height="100"
+      width="100"
+      radius="16"
+      color="#4fa94d"
+      secondaryColor=""
+      ariaLabel="revolving-dot-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
 }
