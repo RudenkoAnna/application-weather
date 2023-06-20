e3d489381865fbc458edee32a1f8218e29c600d6
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-20 21:58:49 +0300

    fixed formatting

--- src/Weather.js
+++ src/Weather.js
@@ -42,12 +42,10 @@ export default function Weather(props) {
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
               alt="Mostly cloudy"
-              className="float-left"
             ></img>
-            <div className="float-left">
-              <span className="temperature">20</span>
-              <span className="unit">°C</span>
-            </div>
+
+            <span className="temperature">20</span>
+            <span className="unit">°C</span>
           </div>
         </div>
         <div className="col-6">
