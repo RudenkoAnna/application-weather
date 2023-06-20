525759cb61268cf64789db5c34d9e68b036223cf
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-20 15:43:32 +0300

    added an experimental allert to check the app

--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,12 @@
+import axios from "axios";
 export default function Weather() {
+  function handleResponse(response) {
+    alert(`the weather in Kyiv is ${response.data.main.temp}C`);
+  }
+  let apiKey = "39d0e5ab9f18d4b08648c0969ea4cd9f";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(handleResponse);
   return <h2> Weather forecast </h2>;
 }
