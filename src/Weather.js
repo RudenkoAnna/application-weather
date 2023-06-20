ae019a455e7ad4d8cb27b6d644b70816f9f06bf9
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-20 21:04:57 +0300

    added content to the front page

--- src/Weather.js
+++ src/Weather.js
@@ -1,25 +1,38 @@
 import axios from "axios";
+import "./Weather.css";
 import { RevolvingDot } from "react-loader-spinner";
+
 export default function Weather(props) {
-  function handleResponse(response) {
-    alert(`the weather in ${props.city}  is ${response.data.main.temp}C`);
-  }
+  function handleResponse(response) {}
   let apiKey = "39d0e5ab9f18d4b08648c0969ea4cd9f";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
 
   axios.get(apiUrl).then(handleResponse);
+
   return (
-    <RevolvingDot
-      height="100"
-      width="100"
-      radius="16"
-      color="#4fa94d"
-      secondaryColor=""
-      ariaLabel="revolving-dot-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-    />
+    <div className="Weather">
+      <h1> Weather in Boryspil</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="Mostly cloudy"
+          ></img>
+          20°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation 24%</li>
+            <li>Humidity 83%</li>
+            <li>Wind:13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
