95a1632b834b0db51db8e8f86927205160ca9dbb
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-21 13:29:10 +0300

    added real data for date

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,3 +1,26 @@
 export default function FormattedDate(props) {
-  return "Wednesday 7am";
+  console.log(props.date);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `${0}hours`;
+  }
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `${0}minutes`;
+  }
+  return (
+    <div>
+      {day} {hours}:{minutes}
+    </div>
+  );
 }
