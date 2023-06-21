0de8951043b0a1750dae7da4898e4f73de2877ba
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-21 11:38:16 +0300

    added the actual data

--- src/Weather.js
+++ src/Weather.js
@@ -1,61 +1,92 @@
+import { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import { RevolvingDot } from "react-loader-spinner";
 
-export default function Weather(props) {
-  function handleResponse(response) {}
-  let apiKey = "39d0e5ab9f18d4b08648c0969ea4cd9f";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+export default function Weather() {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
 
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="Weather">
-      <form>
+    setReady(true);
+  }
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter your city"
+                className="form-control"
+                autoFocus="on"
+              ></input>
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h1> {weatherData.city}</h1>
+        <ul>
+          <li>Wednesday 07:00</li>
+          <li>{weatherData.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter your city"
-              className="form-control"
-              autoFocus="on"
-            ></input>
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1> Weather in Boryspil</h1>
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-              alt="Mostly cloudy"
-            ></img>
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+                alt="Mostly cloudy"
+              ></img>
 
-            <span className="temperature">20</span>
-            <span className="unit">°C</span>
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation 24%</li>
-            <li>Humidity 83%</li>
-            <li>Wind:13km/h</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation 24%</li>
+              <li>Humidity {Math.round(weatherData.humidity)}%</li>
+              <li>Wind:{Math.round(weatherData.wind)}km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "39d0e5ab9f18d4b08648c0969ea4cd9f";
+    let city = "Boryspil";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return (
+      <RevolvingDot
+        height="100"
+        width="100"
+        radius="16"
+        color="#4fa94d"
+        secondaryColor=""
+        ariaLabel="revolving-dot-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    );
+  }
 }
