e1832f30d30fdacec9270f66605ee17e086e723a
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-21 19:59:58 +0300

    added C to F convertion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,8 +1,38 @@
+import { useState } from "react";
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function convertToCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  let fahrenheit = (props.celsius * 9) / 5 + 32;
+  if (unit === "celsius") {
+    return (
+      <div>
+        <span className="temperature">{Math.round(props.celsius)}</span>{" "}
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={convertToFahrenheit} rel="noopener noreferrer">
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <span className="temperature">{Math.round(fahrenheit)}</span>{" "}
+        <span className="unit">
+          <a href="/" onClick={convertToCelsius} rel="noopener noreferrer">
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
 }
