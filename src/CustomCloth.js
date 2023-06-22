be2db8c835a095d7fa80b0548e72577c6b2517d8
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-22 14:24:06 +0300

    fixed issue with the cloth

--- src/CustomCloth.js
+++ src/CustomCloth.js
@@ -3,26 +3,27 @@ export default function CustomCloth(props) {
   let imageSrc;
 
   const temperature = props.temperature;
+
   if (temperature > 20) {
     clothing = "Shorts and T-Shirt";
-    imageSrc = "./customicons/light-wear.png";
+    imageSrc = "/customicons/light-wear.png";
   } else if (temperature >= 15 && temperature <= 20) {
     clothing = "Pants and Hoodie";
-    imageSrc = "./customicons/warm-suit.png";
+    imageSrc = "/customicons/warm-suit.png";
   } else if (temperature >= 5 && temperature < 15) {
     clothing = "Jacket";
-    imageSrc = "./customicons/jacket.png";
+    imageSrc = "/customicons/jacket.png";
   } else if (temperature >= 0 && temperature < 5) {
     clothing = "Coat";
-    imageSrc = "./customicons/coat.png";
+    imageSrc = "/customicons/coat.png";
   } else {
     clothing = "Warm Jacket";
-    imageSrc = "./customicons/winter-jacket.png";
+    imageSrc = "/customicons/winter-jacket.png";
   }
 
   return (
     <div>
-      <img src={imageSrc} alt={clothing} />
+      <img className="cloth" src={imageSrc} alt={clothing} />
     </div>
   );
 }
