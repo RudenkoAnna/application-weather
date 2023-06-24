75a65abeaeae4b5dbd3a533cc8c7209de21f978e
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-24 11:56:34 +0300

    working on weather for next days

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,23 +1,35 @@
 import WeatherIcon from "./WeatherIcon";
 
-export default function weatherForecastDay(props) {
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
   function day() {
-    let date = new Date(props.data.time * 1000);
+    let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
+
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
     return days[day];
   }
+
   return (
-    <div className="WeatherForecast-one-day">
+    <div>
       <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.condition.icon} size={40} />
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
-          {Math.round(props.data.temperature.maximum)}°
+          {maxTemperature()}
         </span>
         <span className="WeatherForecast-temperature-min">
-          {" "}
-          {Math.round(props.data.temperature.minimum)}°
+          {minTemperature()}
         </span>
       </div>
     </div>
