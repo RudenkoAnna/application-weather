5a988f6bf37e4f58aae10ad7a45e37259fe80eb3
Anna Rudrnko <babytreashures@gmail.com>
2023-06-27 14:51:25 +0300

    added server for fetching data  from openweathermap

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const axios = require("axios");
+
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+const PORT = process.env.PORT || 5000;
+
+// Endpoint to fetch weather data
+app.get("/api/weather", async (req, res) => {
+  try {
+    const { city } = req.query;
+    const apiKey = "39d0e5ab9f18d4b08648c0969ea4cd9f";
+
+    const response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    );
+
+    const weatherData = response.data;
+    res.json(weatherData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 });
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
